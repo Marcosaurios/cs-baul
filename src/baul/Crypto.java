6e0bd3ba95d7d075079e59c0aeecb6fa92038739
Natalia Garcia <ngs43@gcloud.ua.es>
2017-11-28 17:39:47 +0100

    RSA y AES junto

--- src/baul/Crypto.java
+++ src/baul/Crypto.java
@@ -1,3 +1,4 @@
+
 package baul;
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,16 +13,30 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-
-// con cambio
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.Cipher;
 
 public class Crypto {
-	
-   static void fileProcessor(int cipherMode,String key,File inputFile,File outputFile){
+	 private static Cipher rsa;
+   static void fileProcessor(int cipherMode,Key key,File inputFile,File outputFile){
 	 try {
-	       Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+	       
 	       Cipher cipher = Cipher.getInstance("AES");
-	       cipher.init(cipherMode, secretKey);
+	       cipher.init(cipherMode, key);
+	       
+	       saveKey(key, "claveAES");
 
 	       FileInputStream inputStream = new FileInputStream(inputFile);
 	       byte[] inputBytes = new byte[(int) inputFile.length()];
@@ -32,18 +47,59 @@ public class Crypto {
 	       FileOutputStream outputStream = new FileOutputStream(outputFile);
 	       outputStream.write(outputBytes);
 
-	       inputStream.close();
-	       outputStream.close();
+	      
+	       
+	       
+//hasta aqui me ha cifrado el archivo en aes y me guarda la clave aes en un archivo
+//que ahora tengo que cifrar
+	       
+	       // Generar el par de claves
+	       KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+	       KeyPair keyPair = keyPairGenerator.generateKeyPair();
+	       PublicKey publicKey = keyPair.getPublic();
+	       PrivateKey privateKey = keyPair.getPrivate();
 
-	    } catch (NoSuchPaddingException | NoSuchAlgorithmException 
-                     | InvalidKeyException | BadPaddingException
-	             | IllegalBlockSizeException | IOException e) {
+	       // Se salva y recupera de fichero la clave publica
+	       saveKey(publicKey, "publickey.dat");
+	       publicKey = loadPublicKey("publickey.dat");
+
+	       // Se salva y recupera de fichero la clave privada
+	       saveKey(privateKey, "privatekey.dat");
+	       privateKey = loadPrivateKey("privatekey.dat");
+
+	       // Obtener la clase para encriptar/desencriptar
+	       
+	       File fichero = new File("claveAES");
+	       FileInputStream fis = new FileInputStream(fichero);
+	       int numBtyes = fis.available();
+	       byte[] bytes = new byte[numBtyes];
+	       fis.read(bytes);
+	       fis.close();	       
+	       rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding"); 
+	      
+	       rsa.init(Cipher.ENCRYPT_MODE, publicKey);
+	       byte[] encriptado = rsa.doFinal(bytes);
+	      
+	       File salida = new File("claveRSA");
+	       
+	       FileOutputStream outputStream2 = new FileOutputStream(salida);
+	       outputStream2.write(encriptado);
+	       //aqui ya he encriptado la clave de aes con rsa y la tengo en un fichero
+	       outputStream.close();
+	       inputStream.close();
+	       
+
+	    } catch (Exception e) {
 		e.printStackTrace();
             }
      }
 	
-     public static void main(String[] args) {
-	String key = "This is a secret";//esto debe generarse random o introducirla el usu
+     public static void main(String[] args) {	
+    	 try {
+	KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+    keyGenerator.init(128);
+    Key key = keyGenerator.generateKey();
+    
 	File inputFile = new File("fichero.txt");//aqui getFiechero
 	
 	//if(true){//cambiar para que esto se haga cuando se active la opcion encriptar
@@ -56,11 +112,11 @@ public class Crypto {
 		File decryptedFile = new File(inputFile.getName()+"-DECRIPTED.txt");
 	//}
 	
+	//ahora tengo que encryptar el archivo que tengo con la clave aes
 	
 	
 	
 	
-	try {
 	     Crypto.fileProcessor(Cipher.ENCRYPT_MODE,key,inputFile,encryptedFile);
 	     Crypto.fileProcessor(Cipher.DECRYPT_MODE,key,encryptedFile,decryptedFile);
 	     System.out.println("Success");
@@ -68,6 +124,38 @@ public class Crypto {
 	     System.out.println(ex.getMessage());
              ex.printStackTrace();
 	 }
-     }
-	
+   }
+     
+    private static PublicKey loadPublicKey(String fileName) throws Exception {
+         FileInputStream fis = new FileInputStream(fileName);
+         int numBtyes = fis.available();
+         byte[] bytes = new byte[numBtyes];
+         fis.read(bytes);
+         fis.close();
+
+         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+         KeySpec keySpec = new X509EncodedKeySpec(bytes);
+         PublicKey keyFromBytes = keyFactory.generatePublic(keySpec);
+         return keyFromBytes;
+      }
+
+      private static PrivateKey loadPrivateKey(String fileName) throws Exception {
+         FileInputStream fis = new FileInputStream(fileName);
+         int numBtyes = fis.available();
+         byte[] bytes = new byte[numBtyes];
+         fis.read(bytes);
+         fis.close();
+
+         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+         KeySpec keySpec = new PKCS8EncodedKeySpec(bytes);
+         PrivateKey keyFromBytes = keyFactory.generatePrivate(keySpec);
+         return keyFromBytes;
+      }
+
+      private static void saveKey(Key key, String fileName) throws Exception {
+         byte[] publicKeyBytes = key.getEncoded();
+         FileOutputStream fos = new FileOutputStream(fileName);
+         fos.write(publicKeyBytes);
+         fos.close();
+      }
 }
